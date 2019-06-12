b81cfda72e0d5ef70eb966dc80c13df1e2495df3
Yogi Prasetyawan <yogi.enjoyers22@gmail.com>
2019-06-13 06:05:30 +0700

    Done: Homepage animation

--- bluesummer/assets/js/script.js
+++ bluesummer/assets/js/script.js
@@ -1,3 +1,9 @@
+// Home sidebar
+function showContent(id) {
+    $('.active-content').removeClass('active-content').css('display', 'none');
+    $(id).fadeIn('500').addClass('active-content');
+}
+
 // Fancybox gallery wedding
 $('[data-fancybox="images"]').fancybox({
   margin : [44,0,22,0],
@@ -17,23 +23,3 @@ $('.wedding-close-navbar').click(function(event) {
     $('.wedding-wrapper').css('margin-left', '0');
     $('.wedding-show-navbar').css('margin-top', '-130px');
 });
-
-// Playing Music
-$('#music').click(function(e) {
-    e.preventDefault();
-    alert('anj')
-    var song = $(this).next('audio').get(0);
-    console.log(song);
-    if (song.paused) {
-        song.play();
-    } else {
-        song.pause();
-    }
-});
-
-
-var audio = '';
-function playAudio(music, id) {
-    var audio = new Audio(music);
-    playingMusic(audio, id);
-}
