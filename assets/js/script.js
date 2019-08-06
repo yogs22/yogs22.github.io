741ce018d1c54c4da94528e5a537560f07c3276d
Yogi Prasetyawan <yogi.enjoyers22@gmail.com>
2019-08-06 08:47:11 +0700

    Fix portfolio style can't load

--- assets/js/script.js
+++ assets/js/script.js
@@ -70,7 +70,7 @@ function openTabs(evt, cityName) {
 
     document.getElementById(cityName).style.display = "block";
     evt.currentTarget.className += " active";
-} 
+}
 
 document.getElementById("defaultOpen").click();
 
@@ -90,14 +90,14 @@ function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
+  if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
+      slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active-slide", "");
   }
-  slides[slideIndex-1].style.display = "block";  
+  slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active-slide";
 }
