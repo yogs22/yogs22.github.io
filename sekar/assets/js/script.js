d0662bcca94a91c2c8b8721c7a8b61e0e850efcc
Yogi Prasetyawan <yogi.enjoyers22@gmail.com>
2019-10-21 21:11:17 +0700

    Add navbar function

--- sekar/assets/js/script.js
+++ sekar/assets/js/script.js
@@ -22,3 +22,52 @@ function showSlides() {
     dots[slideIndex - 1].className += ' active';
     setTimeout(showSlides, 5000); // Change image every 5 seconds
 }
+
+// Scroll ke tujuan
+$('a[data-slide="slide"]').click(function(e){
+    e.preventDefault();
+    var target = $(this).data('slide-target');
+    $("html, body").animate({
+        scrollTop: $(target).offset().top - 80
+    }, 1200);
+});
+
+// Navbar add class if scrolled
+
+function hasScrolled() {
+    var navbar = $('.navbar');
+    if (window.scrollY > 100) {
+        navbar.addClass('scrolled');
+    }else {
+        navbar.removeClass('scrolled');
+    }
+}
+
+$(window).scroll(function() {
+    hasScrolled();
+});
+
+$(window).resize(function() {
+    hasScrolled();
+});
+
+hasScrolled();
+
+// Scroll acive class
+
+$(window).scroll(function() {
+    var windscroll = $(window).scrollTop();
+    if (windscroll >= 100) {
+        $('section').each(function(i) {
+            if ($(this).position().top <= windscroll - 20) {
+                $('navbar li.active').removeClass('active');
+                $('navbar li').eq(i).addClass('active');
+            }
+        });
+
+    } else {
+        $('navbar li.active').removeClass('active');
+        $('navbar li:first').addClass('active');
+    }
+
+}).scroll();
